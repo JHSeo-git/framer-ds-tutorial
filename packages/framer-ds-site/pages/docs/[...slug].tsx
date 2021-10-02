07979c1098a9d6e362fd38ac6ce861e39fedfe54
JHSeo <qnfqnfqnf@gmail.com>
2021-10-02 21:35:22 +0900

    Fix reset docks sub page

--- packages/framer-ds-site/pages/docs/[...slug].tsx
+++ packages/framer-ds-site/pages/docs/[...slug].tsx
@@ -1,27 +1,7 @@
-import React, { useMemo } from 'react'
-import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { getMDXComponent } from 'mdx-bundler/client'
-import Layout from '@components/Layout'
-import { Frontmatter } from '@lib/types/mdx'
-import { Heading } from '@framerds/react'
-import MDXProvider from '@components/mdx/MDXProvider'
-import { getAllFrontmatter } from '@lib/mdx/mdx'
+export type DocsSubPageProps = {}
 
-type DocsSubPageProps = {
-  frontmatter: Frontmatter
-  code: any
-}
-
-const DocsSubPage: NextPage<DocsSubPageProps> = ({ frontmatter, code }) => {
-  const Component = useMemo(() => getMDXComponent(code), [code])
-  return (
-    <Layout>
-      <Heading>{frontmatter.metaTitle}</Heading>
-      <MDXProvider frontmatter={frontmatter}>
-        <Component />
-      </MDXProvider>
-    </Layout>
-  )
+const DocsSubPage = (props: DocsSubPageProps) => {
+  return <div>Docs sub page</div>
 }
 
 export default DocsSubPage
