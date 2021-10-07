ab856eb6b6457ad087ab93c986a31927b176d672
JHSeo <qnfqnfqnf@gmail.com>
2021-10-07 21:41:17 +0900

    Feat app menu routes config

---
+++ packages/framer-ds-site/lib/config/appMenusRoutes.ts
@@ -0,0 +1,26 @@
+export const appMenusRoutes = [
+  {
+    slug: '/design-system',
+    title: 'Design System',
+    description: 'All components of design system.',
+    draft: false,
+  },
+  {
+    slug: '/colors',
+    title: 'Color System',
+    description: 'A color system of design system.',
+    draft: false,
+  },
+  {
+    slug: '/docs',
+    title: 'Documentation',
+    description: 'A complete documentaion of design system.',
+    draft: false,
+  },
+  {
+    slug: '/mdx',
+    title: 'MDX',
+    description: 'MDX Components of design system.',
+    draft: false,
+  },
+]
