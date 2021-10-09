64132c5cb166f175b485ddde53a97e94220600d0
JHSeo <qnfqnfqnf@gmail.com>
2021-10-09 20:10:18 +0900

    Feat responsive routes menu

--- packages/framer-ds-site/components/Header.tsx
+++ packages/framer-ds-site/components/Header.tsx
@@ -1,15 +1,30 @@
 import NextLink from 'next/link'
-import { Box, Container, Flex, Link, Separator, Text } from '@framerds/react'
+import { Box, Container, Flex, Link, Separator, Button } from '@framerds/react'
 import DarkThemeButton from './DarkThemeButton'
 import Logo from './Logo'
 import { appMenusRoutes } from '@lib/config/appMenusRoutes'
 import { useRouter } from 'next/router'
+import { useState, useRef } from 'react'
+import useOnClickOutside from '@hook/useOnClickOutside'
 
 export type HeaderProps = {} & React.ComponentProps<typeof Flex>['css']
 
 const Header = ({ height }: HeaderProps) => {
   const router = useRouter()
   const currentPageSlug = router.asPath
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const menuRef = useRef(null)
+
+  const onClick = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const handleClickOutSide = () => {
+    setMenuOpen(false)
+  }
+
+  useOnClickOutside(menuRef, handleClickOutSide)
 
   return (
     <Box as="header">
@@ -26,37 +41,83 @@ const Header = ({ height }: HeaderProps) => {
           </NextLink>
           <Flex align="center" gap="md">
             <Box
-              as="nav"
+              ref={menuRef}
               css={{
-                display: 'none',
-                '@md': {
-                  display: 'block',
-                },
+                position: 'relative',
+                zIndex: '$5',
               }}
             >
-              <Flex align="center" gap="lg">
-                {appMenusRoutes.map((menu) => (
-                  <NextLink key={menu.slug} href={menu.slug} passHref>
-                    <Link
-                      shape="pill"
-                      // variant="blueLink"
-                      variant={
-                        `/${currentPageSlug.split('/')[1]}` === menu.slug
-                          ? 'blueLink'
-                          : 'contrast'
-                      }
-                      href={menu.slug}
-                      css={{
-                        fontSize: '$sm',
-                      }}
-                    >
-                      {menu.title}
-                    </Link>
-                  </NextLink>
-                ))}
-                <Separator direction="vertical" size="middle" />
-              </Flex>
+              <Button
+                size="small"
+                kind="grayScale"
+                shape="round"
+                state={menuOpen ? 'active' : undefined}
+                ghost
+                onClick={onClick}
+                css={{
+                  '@md': {
+                    display: 'none',
+                  },
+                }}
+              >
+                ROUTES +
+              </Button>
+              <Box
+                as="nav"
+                css={{
+                  position: 'absolute',
+                  top: '100%',
+                  left: 0,
+                  display: menuOpen ? 'block' : 'none',
+                  mt: '$space10',
+                  p: '$lg',
+                  brd: '$mono5',
+                  br: '$radius4',
+                  bc: '$mono1',
+                  '@md': {
+                    position: 'revert',
+                    display: 'block',
+                    mt: 0,
+                    p: 0,
+                    bc: 'inherit',
+                    brd: 'none',
+                    br: 'none',
+                  },
+                }}
+              >
+                <Flex
+                  gap="lg"
+                  css={{
+                    flexDirection: 'column',
+                    '@md': {
+                      flexDirection: 'row',
+                      ac: 'center',
+                    },
+                  }}
+                >
+                  {appMenusRoutes.map((menu) => (
+                    <NextLink key={menu.slug} href={menu.slug} passHref>
+                      <Link
+                        shape="pill"
+                        // variant="blueLink"
+                        variant={
+                          `/${currentPageSlug.split('/')[1]}` === menu.slug
+                            ? 'blueLink'
+                            : 'contrast'
+                        }
+                        href={menu.slug}
+                        css={{
+                          fontSize: '$sm',
+                        }}
+                      >
+                        {menu.title}
+                      </Link>
+                    </NextLink>
+                  ))}
+                </Flex>
+              </Box>
             </Box>
+            <Separator direction="vertical" size="middle" css={{ ml: '$md' }} />
             <DarkThemeButton />
           </Flex>
         </Flex>
